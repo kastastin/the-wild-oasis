1ddca2ad6d886b7a5a36c94cb562c3a51273a07f
kastastin <kostya242421@gmail.com>
2024-01-01 15:52:10 +0200

    Pagination (component, API-Side) + Prefetching react query

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,15 +1,19 @@
+import { useSearchParams } from "react-router-dom";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import styled from "styled-components";
+
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
   display: flex;
-  align-items: center;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const P = styled.p`
-  font-size: 1.4rem;
   margin-left: 0.8rem;
+  font-size: 1.4rem;
 
   & span {
     font-weight: 600;
@@ -22,19 +26,21 @@ const Buttons = styled.div`
 `;
 
 const PaginationButton = styled.button`
-  background-color: ${(props) =>
-    props.active ? " var(--color-brand-600)" : "var(--color-grey-50)"};
-  color: ${(props) => (props.active ? " var(--color-brand-50)" : "inherit")};
-  border: none;
-  border-radius: var(--border-radius-sm);
-  font-weight: 500;
-  font-size: 1.4rem;
-
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   gap: 0.4rem;
   padding: 0.6rem 1.2rem;
+  border: none;
+  border-radius: var(--border-radius-sm);
+
+  background-color: ${(props) =>
+    props.active ? " var(--color-brand-600)" : "var(--color-grey-50)"};
+
+  color: ${(props) => (props.active ? " var(--color-brand-50)" : "inherit")};
+
+  font-size: 1.4rem;
+  font-weight: 500;
   transition: all 0.3s;
 
   &:has(span:last-child) {
@@ -46,8 +52,8 @@ const PaginationButton = styled.button`
   }
 
   & svg {
-    height: 1.8rem;
     width: 1.8rem;
+    height: 1.8rem;
   }
 
   &:hover:not(:disabled) {
@@ -55,3 +61,54 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Pagination({ count }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = !searchParams.get("page")
+    ? 1
+    : Number(searchParams.get("page"));
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1;
+    searchParams.set("page", next);
+    setSearchParams(searchParams);
+  }
+
+  function prevPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1;
+    searchParams.set("page", prev);
+    setSearchParams(searchParams);
+  }
+
+  if (pageCount <= 1) return null;
+
+  return (
+    <StyledPagination>
+      <P>
+        Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
+        <span>
+          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
+        </span>{" "}
+        of <span>{count}</span> results
+      </P>
+
+      <Buttons>
+        <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
+          <HiChevronLeft />
+          <span>Previous</span>
+        </PaginationButton>
+        <PaginationButton
+          onClick={nextPage}
+          disabled={currentPage === pageCount}
+        >
+          <span>Next</span>
+          <HiChevronRight />
+        </PaginationButton>
+      </Buttons>
+    </StyledPagination>
+  );
+}
+
+export default Pagination;
