491619b6288e2917cc5df5157bca12ff14903fb0
kastastin <kostya242421@gmail.com>
2024-01-03 19:20:48 +0200

    displaying stays for current day

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 import Stats from "./Stats";
+import Spinner from "../../ui/Spinner";
 import DurationChart from "./DurationChart";
 import SalesChart from "./SalesChart";
-import Spinner from "../../ui/Spinner";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
@@ -19,7 +20,6 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
   const {
-    stays,
     confirmedStays,
     numDays,
     isLoading: isLoadingStays,
@@ -37,7 +37,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
