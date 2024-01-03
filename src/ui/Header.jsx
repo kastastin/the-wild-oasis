21d5969832025cb122441db7a1296552d608408e
kastastin <kostya242421@gmail.com>
2024-01-03 02:28:07 +0200

    app header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
+  display: flex;
+  gap: 2.4rem;
+  justify-content: flex-end;
+  align-items: center;
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
   background-color: var(--color-grey-0);
@@ -11,7 +16,8 @@ const StyledHeader = styled.header`
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
